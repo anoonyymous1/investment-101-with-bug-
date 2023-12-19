89621700a2a272a698b773ff41bde8e950f01db8
anoonyymous1 <92198109+anoonyymous1@users.noreply.github.com>
2023-12-19 17:13:29 +0800

    Add files via upload

---
+++ investment 101/investment_101App.swift
@@ -0,0 +1,20 @@
+//
+//  investment_101App.swift
+//  investment 101
+//
+//  Created by Celine Tsai on 14/12/23.
+//
+
+import SwiftUI
+
+@main
+struct investment_101App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            MainMenuView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
